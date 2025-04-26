d75485e2159c252ea3a16c69e8532926398bb7df
adi271001 <67856422+adi271001@users.noreply.github.com>
2025-04-27 00:23:26 +0530

    Add files via upload

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Aditya D -  DevOps Engineer at Avathon</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Aditya D Portfolio - DevOps Engineer" />
+            <meta name="description"
+                content="Aditya D's (adi271001) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Aditya D (adi271001)" />
+            <meta name="keywords"
+                content="adi271001, adi271001's portfolio, adi271001 linux, ubuntu portfolio, Aditya D protfolio,Aditya D computer, Aditya D, Aditya ubuntu, Aditya D ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Aditya D Portfolio - DevOps Engineer" />
+            <meta itemProp="description"
+                content="Aditya D's (adi271001) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Aditya D Portfolio - DevOps Engineer" />
+            <meta name="twitter:description"
+                content="Aditya D's (adi271001) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="adi271001" />
+            <meta name="twitter:creator" content="adi271001" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Aditya D Portfolio - DevOps Engineer" />
+            <meta name="og:description"
+                content="Aditya D's (adi271001) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:url" content="http://adi271001.github.io/" />
+            <meta name="og:site_name" content="Aditya D Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
